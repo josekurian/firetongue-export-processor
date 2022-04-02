9f04a78cb6f2283404e476203e75201e54896b67
Phil Wilkins <phil@mp3monster.org>
2022-04-02 17:32:18 +0100

    initial development

---
+++ main.py
@@ -0,0 +1,159 @@
+# FireTongue DB export parser
+#
+#  to run this tool the commandline is py main.py <filename>[.txt|.oxl|.xml]
+#
+# useful resources
+# https://flask.palletsprojects.com/en/2.1.x/
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+
+from typing import Iterator, List
+from flask import Flask, request
+import xml.etree.ElementTree as ET
+import sys
+
+
+argStr = ""
+argLower = ""
+root = None
+
+arg_len = len(sys.argv)
+print("No. args " + str(arg_len))
+print("args " + str(sys.argv))
+
+for i, arg in enumerate(sys.argv):
+    print("arg " + str(i) + " is " + sys.argv[i])
+
+for i, arg in enumerate(sys.argv):
+    argLower = arg.lower()
+    print("arg=" + argLower)
+    if (argLower.endswith(".txt")):
+        fileHandle = open(arg, "r")
+        argStr += fileHandle.read() + "</p>"
+        print("Read " + argLower)
+    elif (argLower.endswith(".xml") or argLower.endswith(".oxl")):
+        print("Read XML " + argLower)
+        tree = ET.parse(argLower)
+        root = tree.getroot()
+    else:
+        argStr += ">" + arg + "< !=  a readable file - will switch to default"
+        argLower = ""
+
+    print(argStr)
+    if (len(argLower) == 0):
+        argLower = "firetongue.oxl"
+
+
+app = Flask(__name__)
+
+if (root == None):
+    tree = ET.parse(argLower)
+
+root = tree.getroot()
+
+
+def extractSearchCriteria(args, baseUrl: str):
+    searchStr = args.get("search")
+
+    print("Search request using >" + str(searchStr) + "<")
+    return searchStr
+
+
+def listout(my_list: list):
+    pretty_list = ""
+    for my_item in my_list:
+        if (isinstance(my_item, str)):
+            pretty_list += str(my_item) + "</br>"
+        elif ((isinstance(my_item, (tuple, list)))):
+            pretty_list += str(my_item[0]) + " : " + str(my_item[1]) + "</br>"
+        else:
+            print("unknown type")
+    return pretty_list
+
+
+def matchedAlbum(name: str, albums: Iterator):
+    """
+    matched_album examines the album names and determines whether they match the search criteria
+
+    Args:
+        name (str): the search criteria - does the name match
+        albums (Iterator): Iterator over the Albums structure of the data file
+
+    Returns:
+        List: List of lists, with the inner list made up of the matching album name and its unique Id
+    """
+    matches = []
+    found = 0
+
+    for album in albums:
+        albumname = album.find('Title').text
+        if ((albumname != None) and (albumname.find(name) > 0)):
+            # matches.append(albumname)
+            matches.append([albumname, album.get("ID")])
+            found += 1
+
+    print(" matches made = " + str(found))
+    return matches
+
+
+@ app.route('/params')
+def params():
+    return argStr
+
+
+@ app.route('/hello')
+def hello():
+    return 'Hello world'
+
+
+@ app.route('/test/')
+def test():
+    root = tree.getroot()
+    search = extractSearchCriteria(request.args, request.base_url)
+    albums = listout(matchedAlbum(search, root.iter('Album')))
+    return albums
+
+
+@ app.route('/test2')
+def test2():
+    result = ""
+    for child in root:
+        result += child.tag + "</p>"
+
+    result += "------</p>"
+    # for child in root.findall('Album'):
+    for child in root.iter('Album'):
+        print(child.find('Title').text)
+        # result += child.get('Title') + "</p>"
+    return result
+
+
+@ app.route('/album')
+def listAlbums():
+    result = ""
+    # locate the album with the specific Id
+    return result
+
+
+@ app.route('/id/album')
+def getAlbum():
+    result = ""
+    # locate the album with the specific Id
+    return result
+
+
+@ app.route('/id/artist')
+def getArtist():
+    result = ""
+    # locate the artist with the specific Id
+    return result
+
+
+@ app.route('/id/recording')
+def getRecording():
+    result = ""
+    # locate the artist with the specific Id
+    return result
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
